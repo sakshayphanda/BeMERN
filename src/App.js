9c45c6aca212ac4e85a1a68ee6ce632c564e35f2
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-06-19 13:23:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>BE MERN STACK</p>
+        <p>React todo list</p>
       </header>
       <div>
         {routing}

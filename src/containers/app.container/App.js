1aa1547ce2a45fd6a19f302ec035cbb747cb125b
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-06-20 15:41:32 +0530

    updated

--- src/containers/app.container/App.js
+++ src/containers/app.container/App.js
@@ -8,14 +8,38 @@ import login from '../login.container/login';
 
 function App() {
   const [posts, setPosts] = useState([]); // react hooks
+  const [newPost, setPost] = useState('');
+  const [currentTime, setTime] = useState(new Date().toLocaleTimeString());
+
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString())
+  }, 1000);
+
   useEffect(() => {
     async function fetch() {
-      const response = await Axios.get('https://jsonplaceholder.typicode.com/todos/');
+      const response = await Axios.get('https://jsonplaceholder.typicode.com/posts/');
       console.log(response.data);
       setPosts(response.data);
     }
     fetch();
-  }, []);
+  }, [newPost]);
+
+  async function hitApi() {
+    await Axios.post('https://jsonplaceholder.typicode.com/posts', {
+      title: newPost,
+      body: 'bar',
+      userId: 1
+    });
+
+    
+
+    setPosts(posts => [
+      {
+      title: newPost,
+      body: 'bar',
+      userId: 1
+      }, ...posts]);
+  }
 
   const routing = (
     <Router>
@@ -32,18 +56,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React todo list</p>
+        <div style={{width: '100%', display: 'flex'}}>React todo list <span style={{marginLeft: 'auto'}}>{currentTime}</span></div>
       </header>
       <div>
         {routing}
       </div>
 
       <div>
+        <input type="text" onBlur={(event) => { setPost(event.target.value) }}/>
+        <p>{newPost}</p>
+        <button onClick={hitApi}>Add</button>
+        <div>
         {
           posts.map(
-            item => (<div>{item.title}</div>)
+            item => (<li>{item.title}</li>)
           )
         }
+        </div>
       </div>
     </div>
   );

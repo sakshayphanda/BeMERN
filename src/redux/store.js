96961f74fb96af21d142e7341a9452aca8a8d4de
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-06-21 18:48:35 +0530

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,4 +4,4 @@ import rootReducer from "./reducers";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-export default createStore(rootReducer, composeWithDevTools(), applyMiddleware(thunk));
+export default createStore(rootReducer, composeWithDevTools( applyMiddleware(thunk)));
